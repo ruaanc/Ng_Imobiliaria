0e54648380fdc2d7e23e16deb03e4e43900879d6
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-21 20:24:30 -0300

    Criação de Usuario e seus componentes

---
+++ src/app/Usuario/new-usuario/new-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-usuario',
+  templateUrl: './new-usuario.component.html',
+  styleUrls: ['./new-usuario.component.css']
+})
+export class NewUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
