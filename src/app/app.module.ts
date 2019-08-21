cc03c4cd1e11bc01fa8ba46dcd531ef36bdc8a3c
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-21 20:21:01 -0300

    Componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { EditClienteComponent } from './cliente/edit-cliente/edit-cliente.compon
 import { ListClientesComponent } from './cliente/list-clientes/list-clientes.component';
 import { NewClienteComponent } from './cliente/new-cliente/new-cliente.component';
 import { ShowClienteComponent } from './cliente/show-cliente/show-cliente.component';
+import { ShowContratoComponent } from './Contrato/show-contrato/show-contrato.component';
+import { EditContratoComponent } from './Contrato/edit-contrato/edit-contrato.component';
+import { ListContratoComponent } from './Contrato/list-contrato/list-contrato.component';
+import { NewContratoComponent } from './Contrato/new-contrato/new-contrato.component';
+import { NewImovelComponent } from './Imovel/new-imovel/new-imovel.component';
+import { EditImovelComponent } from './Imovel/edit-imovel/edit-imovel.component';
+import { ListImovelComponent } from './Imovel/list-imovel/list-imovel.component';
+import { ShowImovelComponent } from './Imovel/show-imovel/show-imovel.component';
+import { ShowUsuarioComponent } from './Usuario/show-usuario/show-usuario.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +23,16 @@ import { ShowClienteComponent } from './cliente/show-cliente/show-cliente.compon
     EditClienteComponent,
     ListClientesComponent,
     NewClienteComponent,
-    ShowClienteComponent
+    ShowClienteComponent,
+    ShowContratoComponent,
+    EditContratoComponent,
+    ListContratoComponent,
+    NewContratoComponent,
+    NewImovelComponent,
+    EditImovelComponent,
+    ListImovelComponent,
+    ShowImovelComponent,
+    ShowUsuarioComponent
   ],
   imports: [
     BrowserModule,
