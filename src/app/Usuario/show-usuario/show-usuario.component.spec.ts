95b8d498de8a10dca466d15eb6a37ba6c49e3966
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-21 20:19:27 -0300

    Crianção de Imovel e seus componentes

---
+++ src/app/Usuario/show-usuario/show-usuario.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowUsuarioComponent } from './show-usuario.component';
+
+describe('ShowUsuarioComponent', () => {
+  let component: ShowUsuarioComponent;
+  let fixture: ComponentFixture<ShowUsuarioComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowUsuarioComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowUsuarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Usuario/show-usuario/show-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-usuario',
+  templateUrl: './show-usuario.component.html',
+  styleUrls: ['./show-usuario.component.css']
+})
+export class ShowUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
