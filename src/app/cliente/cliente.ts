976de1e4d71e2dd467165848768a577746fa6e2f
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-26 20:57:22 -0300

    Atualização

--- src/app/cliente/cliente.ts
+++ src/app/cliente/cliente.ts
@@ -1,15 +1,28 @@
 import { Endereco } from './endereco';
 export class Cliente {
-    public id: number;
+    public id: number
+    private _nomeCompleto: string
+    private _rg: number
+    private _telefone: string
+    private _email: string
+    private _profissao: string
+    private _endereco: Endereco
 
-    constructor(
-       private  nomeCompleto: string,
-       private rg: number,
-       private telefone: string,
-       private email: string,
-       private profissao: string,
-       private endereco: Endereco
 
-    ) {}
+    constructor(theNome: string, theRg: number, theTelefone: string, theEmail: string,theProfissao: string, theEndereco: Endereco) {
+
+        this._nomeCompleto = theNome
+        this._rg = theRg
+        this._telefone = theTelefone
+        this._email = theEmail
+        this._profissao = theProfissao
+        this._endereco = theEndereco
+        this._endereco = theEndereco
+
+    }
+    
+
+    
+    
 
 }
