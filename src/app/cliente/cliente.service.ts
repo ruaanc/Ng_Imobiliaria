8ad352d1bfef6fb813c336d49458c0dd355297d7
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-26 21:39:38 -0300

    Cliente: POST, DELETE, GET e PUT

--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -1,3 +1,5 @@
+import { AppService } from './../app.service';
+import { Cliente } from './cliente';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,44 @@ import { Injectable } from '@angular/core';
 })
 export class ClienteService {
 
-  constructor() { }
+  lastId = 0
+
+  clientes: Cliente[] = []
+
+  constructor(private appService: AppService) { }
+
+  // Simulação do POST /clientes
+  add(cliente: Cliente): Cliente {
+    if (cliente.id == null) {
+      cliente.id = ++this.lastId
+    }
+    this.clientes.push(cliente)
+    return cliente
+  }
+
+  // Simulação do DELETE /clientes/:id
+  delete(id: number) {
+    this.clientes = this.clientes.filter(todo => todo.id !== id)
+  }
+
+  // Simulação do GET /clientes
+  getAll(): Cliente[] {
+    return this.clientes
+  }
+
+  // Simulação do GET /clientes/:id
+  getById(id: number): Cliente {
+    return this.clientes.filter(todo => todo.id === id).pop();
+  }
+
+  // Simulação do PUT /clientes/:id
+  update(novoCliente: Cliente): Cliente {
+    const velhoCliente = this.getById(novoCliente.id)
+    if (!velhoCliente) {
+      return 
+    }
+    Object.assign(velhoCliente, novoCliente)
+    return velhoCliente
+  }
+
 }
