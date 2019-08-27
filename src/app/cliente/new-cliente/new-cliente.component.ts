29e419521b1c848f0064767662fdb1be48c0acce
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-27 11:12:32 -0300

    Cadastro de Cliente

--- src/app/cliente/new-cliente/new-cliente.component.ts
+++ src/app/cliente/new-cliente/new-cliente.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ClienteLocatario } from './../cliente-locatario';
+import { ClienteService } from './../cliente.service';
+import { Cliente } from './../cliente'
+import { Endereco } from './../endereco';
+import { ClienteLocador } from './../cliente-locador';
 
 @Component({
   selector: 'app-new-cliente',
   templateUrl: './new-cliente.component.html',
   styleUrls: ['./new-cliente.component.css']
 })
-export class NewClienteComponent implements OnInit {
+export class NewClienteComponent {
+  
+  tipoCLiente: string
+  cliente: Cliente
+  endereco: Endereco
 
-  constructor() { }
+  constructor(private clienteService: ClienteService ) { }
 
-  ngOnInit() {
+  onSubmit(form: any){
+    this.endereco = new Endereco(form.rua, form.numero, form.cidade, form.estado)
+    if(this.tipoCLiente.toUpperCase() == 'LOCATARIO'){
+      this.cliente = new ClienteLocatario(null, form.name, form.rg, form.telefone, form.email, form.profissao, this.endereco, null)
+    }else{
+      this.cliente = new ClienteLocador(null, form.name, form.rg, form.telefone, form.email, form.profissao, this.endereco, null)
+    }
+    this.cliente = this.clienteService.add(this.cliente)
+    this.clienteService.changeMessage(`Cliente criado com sucesso !`)
   }
 
 }
