4e51b26aee0447d9ce9adc30e7692639b8fe1b45
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-30 11:40:44 -0300

    rotas /clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListClientesComponent } from './Cliente/list-clientes/list-clientes.component';
+
+const routes: Routes = [
+  { path: 'clientes', component: ListClientesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
